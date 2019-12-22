bc65924a9a483d0467c3ec79c691b51c4265dde2
DaniyarHomeW01 <m.doni90.04@gmail.com>
2019-12-22 11:00:05 +0500

    add homework 2

--- jshomework01.js
+++ jshomework01.js
@@ -38,6 +38,7 @@ for (let i = 0; i < 2; i++) {
         appData.expenses[a] = b;
     } else if ((typeof(a)) != 'string' || (typeof(a)) == null || (typeof(b)) == null || a == '' || b == '' || a.length > 50) {
         console.log("eror");
+        i--;
     }
     twoWays++;
 }*/
@@ -52,6 +53,7 @@ for (let i = 0; i < 2; i++) {
         appData.expenses[a] = b;
     } else if ((typeof(a)) != 'string' || (typeof(a)) == null || (typeof(b)) == null || a == '' || b == '' || a.length > 50) {
         console.log("eror");
+        i--;
     }
     twoWays++;
 
