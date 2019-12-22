b502e3e0f7da175cfe7c606408b0e5f5ec1ec88f
DaniyarHomeW01 <m.doni90.04@gmail.com>
2019-12-22 10:51:09 +0500

    add homework 2

--- jshomework01.js
+++ jshomework01.js
@@ -1,6 +1,8 @@
 let money = +prompt("Ваш бюджет на месяц"),
     time = prompt("Введите дату в формате YYYY-MM-DD");
 
+let twoWays = 0;
+
 let appData = {
     budget: money,
     timeData: time,
@@ -10,13 +12,68 @@ let appData = {
     saving:false
 };
 
-let firstAnswer = prompt("Введите обязательную статью расходов в этом месяце"),
-    secondAnswer = +prompt("Во сколько обойдется?");
 
-appData.expenses.firstAnswer = secondAnswer;
 
-let day;
+for (let i = 0; i < 2; i++) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце"),
+        b = prompt("Во сколько обойдется?");
+    
+    if ((typeof(a)) == 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else if ((typeof(a)) != 'string' || (typeof(a)) == null || (typeof(b)) == null || a == '' || b == '' || a.length > 50) {
+        console.log("eror");
+    }
+    
+}
 
-day = appData.budget / 30;
 
-alert("Бюджет на 1 день " + day + " Дата " + appData.timeData);
+/*while (twoWays < 2) {
+    let a = prompt("Введите обязательную статью расходов в этом месяце"),
+        b = prompt("Во сколько обойдется?");
+    
+    if ((typeof(a)) == 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else if ((typeof(a)) != 'string' || (typeof(a)) == null || (typeof(b)) == null || a == '' || b == '' || a.length > 50) {
+        console.log("eror");
+    }
+    twoWays++;
+}*/
+
+
+/*do{
+    let a = prompt("Введите обязательную статью расходов в этом месяце"),
+        b = prompt("Во сколько обойдется?");
+    
+    if ((typeof(a)) == 'string' && (typeof(a)) != null && (typeof(b)) != null && a != '' && b != '' && a.length < 50) {
+        console.log("done");
+        appData.expenses[a] = b;
+    } else if ((typeof(a)) != 'string' || (typeof(a)) == null || (typeof(b)) == null || a == '' || b == '' || a.length > 50) {
+        console.log("eror");
+    }
+    twoWays++;
+
+}while (twoWays < 2)*/
+
+
+appData.moneyPerDay = appData.budget / 30;
+
+alert( "Бюджет на 1 день  " + appData.moneyPerDay + " Дата " + appData.timeData );
+
+if (appData.moneyPerDay < 100) {
+    console.log("Минимальный уровень достатка");
+} else if (appData.moneyPerDay > 100 && appData.moneyPerDay < 2000) {
+    console.log("Средний уровень достатка");
+} else if (appData.moneyPerDay > 2000) {
+    console.log("Высокий уровень достатка");
+} else {
+    console.log("Произошла ошибка");
+};
+
+/*
+1) Строковые-string, числа-number, символы-symbol, undefined, null, логические-boolean, объекты-object, массивы-array
+2)consol.log(таким способом можно вывести инфу в консоль); 
+3) оператор или || пока хоть одна из двух сторон оператора или || равна true, она всегда будет true.
+оператор и && пока две стороны не будут равны true, оператор и && не будет равна true.
+*/
